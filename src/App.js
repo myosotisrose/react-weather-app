17b9b4b78d72cfde045b4154e1acccb383f07aa0
myosotisrose <natalia.pokusa@gmail.com>
2023-03-04 19:37:14 +0100

    add bootstrap

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Natalia Pokusa and is{" "}
-        <a href="https://github.com/myosotisrose/react-weather-app" target="_blank">open-sourced on GitHub</a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by Natalia Pokusa and is{" "}
+          <a href="https://github.com/myosotisrose/react-weather-app" target="_blank">open-sourced on GitHub</a>
+        </footer>
+      </div>
     </div>
   );
 }
