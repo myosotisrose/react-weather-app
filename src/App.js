c58557d2fb82e6d07cc024ed32c5f6fb11d2192b
myosotisrose <natalia.pokusa@gmail.com>
2023-03-04 20:01:13 +0100

    add html structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1>Weather App</h1>
+        <Weather />
         <footer>
           This project was coded by Natalia Pokusa and is{" "}
           <a href="https://github.com/myosotisrose/react-weather-app" target="_blank">open-sourced on GitHub</a>
