150337f3ff15d31835ead07a74706040566b5316
myosotisrose <natalia.pokusa@gmail.com>
2023-03-04 18:29:51 +0100

    add GitHub repo url

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
 import './App.css';
 
 export default function App() {
-  return <div className="App">Weather App</div>
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Natalia Pokusa and is{" "}
+        <a href="https://github.com/myosotisrose/react-weather-app" target="_blank">open-sourced on GitHub</a>
+      </footer>
+    </div>
+  );
 }
