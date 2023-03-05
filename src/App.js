1eed9c1aca14df9e10bdbabdc47527f59c2914ce
myosotisrose <natalia.pokusa@gmail.com>
2023-03-05 16:23:18 +0100

    add norefferer attr

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ export default function App() {
         <Weather defaultCity="New York" />
         <footer>
           This project was coded by Natalia Pokusa and is{" "}
-          <a href="https://github.com/myosotisrose/react-weather-app" target="_blank">open-sourced on GitHub</a>
+          <a href="https://github.com/myosotisrose/react-weather-app" target="_blank" rel="noreferrer">open-sourced on GitHub</a>
         </footer>
       </div>
     </div>
